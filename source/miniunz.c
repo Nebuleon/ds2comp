e97b3170c3505cb08028563ba8b6b0b8438cbe7b
Nebuleon Fumika <nebuleon@alakazam>
2013-02-23 14:56:52 -0500

    Properly increase the current archive member number even if the current archive member is a directory. This ends a .zip archive extraction at X/X members done, not (X-1)/X.

--- source/miniunz.c
+++ source/miniunz.c
@@ -129,10 +129,10 @@ int ZipUncompress(file)
             return error(msg[MSG_ERROR_COMPRESSED_FILE_READ]) != DS2COMP_RETRY;
         }
 
+        UpdateProgressChangeFile(++CurrentFile, Filename, file_info.uncompressed_size);
+
         if (Filename[0] && Filename[strlen(Filename) - 1] == '/')
             goto next_file;
-
-        UpdateProgressChangeFile(++CurrentFile, Filename, file_info.uncompressed_size);
 
         char outfile[MAX_PATH + 1];
         strcpy(outfile, Path);
